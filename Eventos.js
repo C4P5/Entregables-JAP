2de1770ef86f3dc7667e3207013f916eb2767dfe
CAPS <caprioli.santiago@gmail.com>
2023-08-18 12:46:19 -0700

    Solución Click

--- Eventos.js
+++ Eventos.js
@@ -1,12 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var div = document.getElementsByTagName("div")[0];
-  
- div.addEventListener("click", () => {
-  Swal.fire({
-    title: 'Hola!',
-    text: 'Soy el DIV :)',
-    icon: 'info',
-    confirmButtonText: 'Ok'
-  });
+    var div = document.getElementById("miDiv");
+
+    div.addEventListener("click", (event) => {
+        if (event.target.tagName === "BUTTON") {
+            Btn();
+        } else if (event.target.tagName === "DIV") {
+            mostrarAlertaDiv();
+        }
+    });
 });
-});
+
+function Btn() {
+    Swal.fire({
+        title: 'Saludos!',
+        text: '👋👋👋',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+    });
+}
+
+function mostrarAlertaDiv() {
+    Swal.fire({
+        title: '¡Hola!',
+        text: 'Soy el div :)',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+    });
+}
